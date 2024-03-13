4ac6b676d3cafeab8d8f54d3734982ffa43f6104
Marin Prusac <prusac.marin@gmail.com>
2024-03-13 19:23:37 +0100

    New terminal progression shader

---
+++ Server/Gameplay/TerminalProgress.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var terminal_name = "?"
+var max_value = 1
+var current_value = 0
+
+func _ready():
+	$TextureRect/Label.text = terminal_name
+	$TextureProgress.max_value = max_value
+	$TextureProgress.value = current_value
+
+func set_value(value):
+	current_value = value
+	$TextureProgress.value = value
+
+func set_endagered(is_endangered):
+	pass
