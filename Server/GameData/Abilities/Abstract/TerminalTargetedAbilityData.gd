55799b7b5bd26b267bc47aa1431be2266d188a57
Marin Prusac <prusac.marin@gmail.com>
2024-07-14 21:55:41 +0200

    Crack ability bug fix

--- Server/GameData/Abilities/Abstract/TerminalTargetedAbilityData.gd
+++ Server/GameData/Abilities/Abstract/TerminalTargetedAbilityData.gd
@@ -9,7 +9,7 @@ func is_valid_target(terminal):
 	return terminal != last_target
 
 func can_use(player, terminal, port):
-	return is_valid_target(terminal) and not port.solved and not port.name in player.known_ports and not is_on_cooldown()
+	return is_valid_target(terminal) and not port.name in player.known_ports and not is_on_cooldown()
 
 func to_dict(player, terminals):
 	var usable_on = []
--- Server/GameData/Abilities/CrackAbilityData.gd
+++ Server/GameData/Abilities/CrackAbilityData.gd
@@ -3,3 +3,4 @@ class_name CrackAbilityData
 
 func _init(cooldown).("Crack", cooldown):
 	pass
+
